ec18fac2bec708e258bc488646b9032b55cad22c
kartik01112004 <kartikgoel018@gmail.com>
2024-07-31 18:51:57 +0530

    starting tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,8 +2,19 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 
 contract FundMeTest is Test {
-    function setUp() external {}
+    uint256 n = 1;
+
+    function setUp() external {
+        n = 2;
+    }
+
+    function testDemo() public view {
+        console.log(n);
+        console.log(n);
+        console.log(n);
+        assertEq(n, 2);
+    }
 }
